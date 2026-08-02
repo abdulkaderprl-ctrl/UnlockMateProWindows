9825e4554f6ff2d236a74ae64a6e189a9a473c69
Abdul Kader <abdulkaderprl@gmail.com>
2026-08-02 06:36:31 +0600

    Phase 2 - UI, Theme, Architecture and Bug Fixes Complete

--- obj/Release/net8.0-windows/UnlockMatePro.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/UnlockMatePro.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Professional Android Device Management, ADB/Fastboot Suite & Screen Control Platf" +
     "orm")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fdb804efa5c5a724a2b7b9ca151082ad2f307d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7aa085e2a1ff4b86e3dd16b5d0d07e0788af844")]
 [assembly: System.Reflection.AssemblyProductAttribute("Unlock Mate Pro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Unlock Mate Pro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
